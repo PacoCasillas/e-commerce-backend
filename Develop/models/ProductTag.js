f09c8bc2ac825eef724cc681b48d1d317a47c074
PacoCasillas <pacocasillasmx@gmail.com>
2023-06-03 13:29:26 -0400

    set up database models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,41 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
+
+// Import the Product model
+const Product = require('./Product')
+
+// Import the Tag model 
+const Tag = require('./Tag')
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // Reference the Product model
+        model: Product,
+        // Reference the 'id' column of the Product model
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // Reference the Tag model
+        model: Tag,
+        // Reference the 'id' column of the Tag model
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
